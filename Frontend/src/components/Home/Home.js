c552b121f03b3fd1d1f7b2645c101c4da2720998
abdulsalamcodes <abdulsalamabodunrin369@gmail.com>
2021-08-28 16:48:12 +0100

    test

--- Frontend/src/components/Home/Home.js
+++ Frontend/src/components/Home/Home.js
@@ -4,7 +4,11 @@ import styles from './Home.module.scss';
 const Home = () => {
     return (
         <div className={styles.title}>
+            <div>
             <h1>The Team Seneca</h1>
+            <p>Whiteboard plugin</p>
+
+            </div>
         </div>
     )
 }
