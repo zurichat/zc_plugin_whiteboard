27e21a1b4c8a2b3ecd598161ef782a59f1301317
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-09-01 23:01:11 +0100

    Submission for issue #250 by Dorcas Bamisile

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../App.css";
+import WhiteboardCanvas from "../components/WhiteboardCanvas";
 
 function Whiteboard() {
   return (
     <div className="App">
+      <WhiteboardCanvas />
       <header className="App-header">Zuri whiteboard plugin</header>
     </div>
   );
