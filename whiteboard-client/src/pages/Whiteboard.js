5175ea57980b67b0d64824fe2b227ed3eaa55bd9
abdulsalamcodes <abdulsalamabodunrin369@gmail.com>
2021-09-01 15:07:55 +0100

    feat: Implement the participant avatar view

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "../App.css";
 import WhiteboardCanvas from "../components/Whiteboard-Canvas/WhiteboardCanvas";
+import Participants from "../components/Participants";
+import ToolBar from "../components/ToolBar";
 
 function Whiteboard() {
   return (
     <div className="App">
       <WhiteboardCanvas />
       <header className="App-header">Zuri whiteboard plugin</header>
+      {/* <header className="App-header">Zuri whiteboard plugin</header> */}
+      <Participants />
+      <ToolBar />
     </div>
   );
 }
