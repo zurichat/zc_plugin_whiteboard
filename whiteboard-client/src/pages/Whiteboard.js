3a0eb6dee7b8859bd3f3485a0b5cde49d2b872b2
kenny-67 <41513164+kenny-67@users.noreply.github.com>
2021-09-02 12:32:37 +0100

    showed created component to screen merged develop and main

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../App.css";
 import WhiteboardCanvas from "../components/Whiteboard-Canvas/WhiteboardCanvas";
 import Participants from "../components/Participants";
+<<<<<<< HEAD
 import ToolBar from "../components/ToolBar";
+=======
+import SideBar from "../components/sidebar-component/sidebar.js";
+>>>>>>> e0e05d2... showed created component to screen, merged develop and main
 
 function Whiteboard() {
   return (
@@ -11,7 +15,11 @@ function Whiteboard() {
       <header className="App-header">Zuri whiteboard plugin</header>
       {/* <header className="App-header">Zuri whiteboard plugin</header> */}
       <Participants />
+<<<<<<< HEAD
       <ToolBar />
+=======
+      <SideBar />
+>>>>>>> e0e05d2... showed created component to screen, merged develop and main
     </div>
   );
 }
