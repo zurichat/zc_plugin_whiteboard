c047c5ba5c02217d0c2225cef995ad2fd88e067b
Diseyi <filomiinaa@gmail.com>
2021-09-02 16:55:59 +0100

    updated the whiteboardjs file

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -5,6 +5,7 @@ import ColorPicker from "./components/colorpicker/ColorPicker";
 function Whiteboard() {
   return (
     <div className="App">
+      {/* <header className="App-header">Zuri whiteboard plugin</header> */}
       <ColorPicker />
     </div>
   );
