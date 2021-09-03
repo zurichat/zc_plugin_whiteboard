27d3863993f220bc9b3f73d7dd8625a523b2aaaa
dretoba91e <64743870+dretoba91@users.noreply.github.com>
2021-09-03 08:02:55 +0100

    toolbar 5 issue 268

---
+++ whiteboard-client/src/components/toolbar-5/Toolbar5.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Toolbar5.css";
+import bullets from "./asset/bullets.svg";
+import trash from "./asset/trash.svg";
+import eclipse from "./asset/eclipse.svg";
+import line_icon from "./asset/line_icon.svg";
+import link from "./asset/link.svg";
+import pin from "./asset/pin.svg";
+import twosquares from "./asset/twosquares.svg";
+import active from "./asset/active.svg";
+
+function Toolbar5() {
+  return (
+    <div className="toobar">
+      <div className="toolContainer">
+        <div className="tools">
+          <img src={pin} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={eclipse} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={line_icon} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={active} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={twosquares} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={link} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={trash} alt="" />
+        </div>
+
+        <div className="tools">
+          <img src={bullets} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Toolbar5;
--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../App.css";
 import Participants from "../components/Participants";
+import Toolbar5 from "../components/toolbar-5/Toolbar5";
 
 function Whiteboard() {
   return (
     <div className="App">
       {/* <header className="App-header">Zuri whiteboard plugin</header> */}
       <Participants />
+
+      {/* issues 268 Tool bar  */}
+      <Toolbar5 />
     </div>
   );
 }
