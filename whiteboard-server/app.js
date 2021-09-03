18430994a8af5ee8beb98392d4800af563259990
Chijioke Udogu <56207402+Bootspace@users.noreply.github.com>
2021-09-03 11:54:06 +0100

    Update app.js
    Commented out some middleware

--- whiteboard-server/app.js
+++ whiteboard-server/app.js
@@ -18,11 +18,11 @@ app.get("/", (req, res) => {
 });
 
 //Error handling
-app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not Found");
+//   error.status = 404;
+//   next(error);
+// });
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
