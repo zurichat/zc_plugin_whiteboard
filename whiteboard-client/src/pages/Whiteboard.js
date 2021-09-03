7f3ad6439b03bb9c51c3646cacf97ca44835054d
Oluwakayode Adesina <adesinak4@gmail.com>
2021-09-03 23:10:43 +0100

    Update Whiteboard.js

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "../App.css";
-//import Participants from "../components/Participants";
 
 function Whiteboard() {
   return (
     <div className="App">
-      //{/* <header className="App-header">Zuri whiteboard plugin</header> */}
-      //<Participants />
     </div>
   );
 }
