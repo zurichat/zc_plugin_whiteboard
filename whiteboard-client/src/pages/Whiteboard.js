840d61f49b34b54c241ad0503e43b263746c2fe3
Diseyi <filomiinaa@gmail.com>
2021-09-03 08:31:44 +0100

    colorpicker component

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -5,6 +5,7 @@ import Participants from "../components/Participants";
 import ToolBar from "../components/ToolBar";
 import SideBar from "../components/sidebar-component/Sidebar.js";
 import ShapeSelector from "../components/ShapesSelector/ShapeSelector";
+import ColorPicker from "../components/Colorpicker/Colorpicker";
 
 function Whiteboard() {
   return (
@@ -14,6 +15,7 @@ function Whiteboard() {
       {/* <header className="App-header">Zuri whiteboard plugin</header> */}
       <Participants />
       <ToolBar />
+<ColorPicker />
       <ShapeSelector />
     </div>
   );
