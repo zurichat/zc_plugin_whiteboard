4183794aaed9dfb47b684ef0c289569962e74d8a
liza <lizaessien23@gmail.com>
2021-09-05 00:36:01 +0300

    edited styles

--- whiteboard-client/src/components/Zoom/Zoom.js
+++ whiteboard-client/src/components/Zoom/Zoom.js
@@ -6,23 +6,35 @@ import "./Zoom.css";
 
 
 const Zoom = () => {
+
+    const zoomOut=()=>{
+        alert('zoomout')
+    }
+
+    const zoomIn=()=>{
+        alert('zoomin')
+    }
+
+
     return (
         <div className="zoom-area">
         <div className="zoom-container">
          <div className="zoom-amount">100%</div>
-         <div className="zoom-out">
+         <div onClick = {zoomOut} className="zoom-out">
             <img src={zoomout} alt={zoomout}/>
          </div>
-         <div className="zoom-in">
-         <img src={zoomin} alt={zoomin}/>
+         <div onClick = {zoomIn}  className="zoom-in">
+         <img class ="zoom" src={zoomin} alt={zoomin} />
               </div>  
 
               <div className="reset">
-                  <button className="reset-btn">reset</button>
+                  <button className="reset-btn">Reset</button>
                </div>   
         </div>
         </div>
     )
 }
+
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
 export default Zoom;
