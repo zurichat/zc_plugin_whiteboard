028b0f4c6fb494c9d3e030721effd8f74ceea526
Precious Akams <preciousakams@yahoo.com>
2021-09-04 22:59:09 +0100

    update

--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "../App.css";
+import WhiteboardCanvas from "../components/Whiteboard-Canvas/WhiteboardCanvas";
+import Participants from "../components/Participants";
+import ToolBar from "../components/ToolBar";
+import SideBar from "../components/sidebar-component/sidebar.js";
+import ShapeSelector from "../components/ShapesSelector/ShapeSelector";
+import SavedFileView from "../components/SavedFileView/SavedFileView";
+import ColorPicker from "../components/Colorpicker/Colorpicker";
 
 function Whiteboard() {
   return (
     <div className="App">
+      <SideBar />
+      <WhiteboardCanvas />
+      {/* <header className="App-header">Zuri whiteboard plugin</header> */}
+      <Participants />
+      <ToolBar />
+      <ColorPicker />
+      <ShapeSelector />
+      <SavedFileView />
     </div>
   );
 }
