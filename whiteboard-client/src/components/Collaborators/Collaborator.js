bac0d2734f506e2b0cf9101aceed7ec0b4423fce
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2021-09-10 22:37:49 +0100

    create collaborators

---
+++ whiteboard-client/src/components/Collaborators/Collaborator.js
@@ -0,0 +1,25 @@
+import React from "react"
+import styled from "styled-components";
+
+const collab = styled.div`
+position:relative;
+background:#18A0FB;
+color:#fff;
+display:flex;
+align-items:center;
+justify-content:center;
+font-size:.8em;
+width:82px;
+height:36px;
+margin:10px;
+border-top-left-radius:4px;
+border-top-right-radius:20px;
+border-bottom-right-radius:20px;
+border-bottom-left-radius:20px;
+background:${props => props.order == "first" ? "#18A0FB" : props.order == "second" ? "#FEA162" : props.order == "third" ? "#FE6262":"green" }
+
+`
+export default collab;
+// #18A0FB
+// #FEA162
+// #FE6262
--- whiteboard-client/src/pages/Whiteboard.js
+++ whiteboard-client/src/pages/Whiteboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components"
 import "../App.css";
 // import WhiteboardCanvas from "../components/Whiteboard-Canvas/WhiteboardCanvas";
 import ToolBar from "../components/ToolBar/ToolBar";
@@ -6,13 +7,38 @@ import SearchBar from "../components/SearchBar/SearchBar";
 import Zoom from "../components/Zoom/Zoom";
 import Header from "../components/Header/Header";
 import Board from "../components/Whiteboard-Canvas/Board";
+import Collaborator from "../components/Collaborators/Collaborator";
+import Pointer from "../components/Collaborators/Pointer"
+
+
+const Temp = styled.div`
+  display:grid;
+  grid-template-columns:repeat(2,100px)
+`
 
 function Whiteboard() {
+  const collaborators = [
+    {id:1,name:"james",order:"first"},
+    {id:2,name:"Kamzzy",order:"second"},
+    {id:3,name:"Tamara",order:"third"},
+    {id:4,name:"Snow",order:"fourth"}
+  ]
   return (
     <div className="App">
       <SearchBar />
       <Header />
       {/* <WhiteboardCanvas /> */}
+      <Temp>
+      {collaborators.map(person=>{
+        return (
+         <Collaborator key={person.id} order={person.order} >
+          <Pointer order={person.order} className="fas fa-mouse-pointer"/>
+          {person.name}
+        </Collaborator>
+        )
+        
+      })}
+      </Temp>
       <Board />
       <ToolBar />
       <Zoom />
