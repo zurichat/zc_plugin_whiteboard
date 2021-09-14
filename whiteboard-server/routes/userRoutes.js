5bb2e7dc362d76a778ab6a53003d3aa4636d840c
Precious Akams <preciousakams@yahoo.com>
2021-09-14 10:41:03 +0100

    updates

--- whiteboard-server/routes/userRoutes.js
+++ whiteboard-server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
 const userController = require("../controllers/userController");
 const rolePermission = require("../middlewares/rolePermission");
