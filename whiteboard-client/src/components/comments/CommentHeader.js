8d6549e8c1008317e1c1e52ea5dc8d1225849f92
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-09-22 21:41:01 +0100

    Added Toggle to the Comment Component

--- whiteboard-client/src/components/comments/CommentHeader.js
+++ whiteboard-client/src/components/comments/CommentHeader.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import close from "./assets/close.svg"
+import { useState } from 'react';
+import CommentContainer from './CommentContainer';
 
 
 const CommentHeader = () => {
+    const [show, setShow] = useState(true)
+
     return (
         <div>
             <div className="div-header">
                 <h4 className="h4-header">Comments</h4>
-                <img alt="close" src={close} className="close-icon-header" />
+                <img alt="close" src={close} onClick={() => setShow(false)} className="close-icon-header" />
             </div>
+
+            {
+                show?<div>
+                        <CommentContainer />
+                </div>:null
+            }
         </div>
     )
 }
