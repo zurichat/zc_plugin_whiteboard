2b452318679fb6356a2d20f1f55859581087f069
Precious Akams <preciousakams@yahoo.com>
2021-10-27 11:54:37 +0100

    cleanup

--- whiteboard-server/routes/router.js
+++ whiteboard-server/routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = require("express").Router();
-const InformationController = require("../controllers/sidebar_info");
+const InformationController = require("../controllers/sidebarController");
 
 // Sidebar route
 router.get("/sidebar", InformationController.SidebarInformation);
