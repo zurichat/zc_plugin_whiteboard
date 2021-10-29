484c60a7432974a62927785fcc295e63daaa2db4
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-10-29 12:46:45 +0100

    config

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const router = require("./routes/router");
-const { PORT } = require("./config");
+const router = require("./src/routes/router");
+const { PORT } = require("./src/config");
 require("dotenv").config();
 
 //initialize express
@@ -16,32 +16,32 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.use(express.static(path.join(__dirname, "downloader")));
 
 app.get("/download", (req, res) => {
-  res.sendFile(path.join(__dirname, "downloader", "downloadtest.html"));
+    res.sendFile(path.join(__dirname, "downloader", "downloadtest.html"));
 });
 
 app.use(express.static(path.join(__dirname, "../whiteboard-root/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../whiteboard-root/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../whiteboard-root/dist/index.html"));
 });
 
 //Error handling
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
 });
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
 });
 
 // The server should start listening
 app.listen(PORT, () => {
-  console.log(`Server is listening on port http://127.0.0.1:${PORT}`);
+    console.log(`Server is listening on port http://127.0.0.1:${PORT}`);
 });
